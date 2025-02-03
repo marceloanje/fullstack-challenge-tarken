a47dc7212096f73bb419a6c040c61d2b077c5890
marceloanje <marcelo.anje@outlook.com>
2025-02-03 14:58:15 -0300

    feat(library): Initial structure for library module

--- backend/src/library/library.service.ts
+++ backend/src/library/library.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Library } from './library.entity';
+import { SearchService } from '../search/search.service';
+import { Movie } from '../movie/movie.entity';
 
 @Injectable()
-export class LibraryService {}
+export class LibraryService {
+  constructor(
+    @InjectRepository(Library)
+    private readonly libraryRepository: Repository<Library>,
+    private readonly searchService: SearchService,
+  ) {}
+
+  async createLibraryIfNotExists(): Promise<Library> {
+    let library = await this.libraryRepository.findOne({ where: {} });
+
+    if (!library) {
+      library = this.libraryRepository.create({});
+      await this.libraryRepository.save(library);
+    }
+
+    return library;
+  }
+
+  async addMovieToLibrary(movieId: string): Promise<Library> {
+    const movie = await this.searchService.getMovieById(movieId);
+
+    if (!movie) {
+      throw new NotFoundException('Movie not found');
+    }
+
+    const library = await this.createLibraryIfNotExists();
+
+    library.movie = movie;
+    await this.libraryRepository.save(library);
+
+    return library;
+  }
+}
