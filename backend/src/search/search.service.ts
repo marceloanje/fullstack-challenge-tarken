abe258f24a72b863cf795b079439d9d1534fe558
marceloanje <marcelo.anje@outlook.com>
2025-02-02 22:18:51 -0300

    feat(api): Implements logic to call external API for movie search

---
+++ backend/src/search/search.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { catchError, map } from 'rxjs';
+import { throwError } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class SearchService {
+    constructor(private readonly HttpService: HttpService) {}
+
+    async searchMovie(query: string): Promise<any> {
+        const apiUrl = `http://www.omdbapi.com/?apikey=f993b6c&s=${query}`;
+    
+        return firstValueFrom(
+            this.HttpService.get<any>(apiUrl)
+                .pipe(
+                    map(response => response.data),
+                    catchError(error => {
+                        console.error('Error querying external API:', error.message);
+                        return throwError(() => new Error('Failed to fetch movie data'));
+                    })
+                )
+        );
+    }
+}
